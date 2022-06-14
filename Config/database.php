f5573da3c4a17290481feb4eb6e71a968cb3a7a8
YHC <yanhaochuang@qq.com>
2022-06-14 10:51:59 +0800

    优化数据库配置文件结构，新增默认数据库连接

--- Config/database.php
+++ Config/database.php
@@ -7,80 +7,84 @@
  */
 
 return [
-    /**
-     * 从env配置文件读取
-     */
-    'default' => env('database'),
+    'default' => 'default',
 
-    /**
-     *  连接名称 default
-     */
-    'mysql' => [
-        'host' => 'host.docker.internal',
-        'port' => 3306,
-        'database' => 'horseloft',
-        'username' => 'root',
-        'password' => '123456',
-        'driver' => 'mysql'
-    ],
+    'connections' => [
+        /**
+         * 从env配置文件读取
+         */
+        'default' => env('database'),
 
-    /**
-     * 读写分离 / 多数据源配置
-     */
-    'read_and_write' => [
-        'host' => 'host.docker.internal',
-        'port' => 3306,
-        'database' => 'horseloft',
-        'username' => 'root',
-        'password' => '123456',
-        'driver' => 'mysql',
-        'read' => [
+        /**
+         *  连接名称 default
+         */
+        'mysql' => [
+            'host' => 'host.docker.internal',
+            'port' => 3306,
+            'database' => 'horseloft',
+            'username' => 'root',
+            'password' => '123456',
+            'driver' => 'mysql'
+        ],
+
+        /**
+         * 读写分离 / 多数据源配置
+         */
+        'read_and_write' => [
             'host' => 'host.docker.internal',
             'port' => 3306,
             'database' => 'horseloft',
             'username' => 'root',
             'password' => '123456',
             'driver' => 'mysql',
-            [
+            'read' => [
                 'host' => 'host.docker.internal',
                 'port' => 3306,
                 'database' => 'horseloft',
                 'username' => 'root',
                 'password' => '123456',
                 'driver' => 'mysql',
+                [
+                    'host' => 'host.docker.internal',
+                    'port' => 3306,
+                    'database' => 'horseloft',
+                    'username' => 'root',
+                    'password' => '123456',
+                    'driver' => 'mysql',
+                ],
+                [
+                    'host' => 'host.docker.internal',
+                    'port' => 3306,
+                    'database' => 'horseloft',
+                    'username' => 'root',
+                    'password' => '123456',
+                    'driver' => 'mysql',
+                ]
             ],
-            [
-                'host' => 'host.docker.internal',
-                'port' => 3306,
-                'database' => 'horseloft',
-                'username' => 'root',
-                'password' => '123456',
-                'driver' => 'mysql',
-            ]
-        ],
-        'write' => [
-            'host' => 'host.docker.internal',
-            'port' => 3307,
-            'database' => 'horseloft',
-            'username' => 'root',
-            'password' => '123456',
-            'driver' => 'mysql',
-            [
+            'write' => [
                 'host' => 'host.docker.internal',
                 'port' => 3307,
                 'database' => 'horseloft',
                 'username' => 'root',
                 'password' => '123456',
                 'driver' => 'mysql',
-            ],
-            [
-                'host' => 'host.docker.internal',
-                'port' => 3307,
-                'database' => 'horseloft',
-                'username' => 'root',
-                'password' => '123456',
-                'driver' => 'mysql',
+                [
+                    'host' => 'host.docker.internal',
+                    'port' => 3307,
+                    'database' => 'horseloft',
+                    'username' => 'root',
+                    'password' => '123456',
+                    'driver' => 'mysql',
+                ],
+                [
+                    'host' => 'host.docker.internal',
+                    'port' => 3307,
+                    'database' => 'horseloft',
+                    'username' => 'root',
+                    'password' => '123456',
+                    'driver' => 'mysql',
+                ]
             ]
         ]
-    ]
+    ],
 ];

337cbc4fc5a90924189f6a00eaecbef2e3178810
YHC <yanhaochuang@qq.com>
2022-06-29 14:10:55 +0800

    优化基础方法加载

--- Core/core.php
+++ Core/core.php
@@ -3,6 +3,4 @@
 $loader = require_once dirname(__DIR__) . '/vendor/autoload.php';
 $loader->addPsr4('Application' . '\\', dirname(__DIR__) . '/Application');
 
-require_once dirname(__DIR__) . '/Core/functions.php';
-
 $_HORSELOFT_CORE_CONTAINER_ = [];

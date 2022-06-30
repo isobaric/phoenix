8d7c69ce248129ab824a1dc3bb90e03f1d12c86f
YHC <yanhaochuang@qq.com>
2022-06-30 15:52:32 +0800

    补充注释说明

--- Application/Runtime/Exception.php
+++ Application/Runtime/Exception.php
@@ -8,15 +8,9 @@ use Throwable;
 class Exception
 {
     /**
-     * 默认异常处理类|当前类文件为项目基础组件
-     *
-     * 在命名空间 Application\Runtime 中自定义异常处理类
-     *   例：RuntimeException::class 捕捉RuntimeException异常并处理
-     *   例：LoginException::class 捕捉LoginException异常并处理
+     * 默认异常处理类
      *
      * 当前类中必须存在handle()方法
-     *
-     * handle()方法必须有两个参数
      *
      * handle()方法的第一个参数：Horseloft\Bridge\Builder\Request $request
      *
@@ -24,10 +18,9 @@ class Exception
      *
      * handle()方法的返回值将作为本次接口的响应值输出
      *
-     * 如果在当前类中出现了异常信息 则当前异常的处理无效，服务将使用默认的异常处理
-     *
      * @param Request $request
      * @param Throwable $e
+     *
      * @return mixed
      */
     public static function handle(Request $request, Throwable $e)

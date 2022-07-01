8d5a9ce7a78605c15f272c4533b9f3bf80aaa4a1
YHC <yanhaochuang@qq.com>
2022-07-01 09:45:16 +0800

    修复配置错误

--- Config/cors.php
+++ Config/cors.php
@@ -7,15 +7,16 @@
  */
 
 return [
+
     //允许的请求网址
-    'Access-Control-Allow-Origin: *',
+    'allow_origin' => '*',
 
     //允许的请求头信息
-    'Access-Control-Allow-Headers: *',
+    'allow_headers' => '*',
 
     //允许的请求类型
-    'Access-Control-Allow-Methods: GET,POST',
+    'allow_methods' => 'GET,POST',
 
     //允许缓存时长 86,400秒（1天）
-    'Access-Control-Max-Age: 86400'
+    'max_age' => '86400'
 ];

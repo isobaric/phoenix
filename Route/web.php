829d15747aabc10ba067839aa5d6af96bfa0e71f
YHC <yanhaochuang@qq.com>
2022-08-24 15:33:11 +0800

    添加调试路由

--- Route/web.php
+++ Route/web.php
@@ -2,9 +2,13 @@
 
 use Horseloft\Phalanx\Builder\Route;
 
-Route::group(['prefix' => '/index', 'namespace' => '', 'interceptor' => ['Auth', 'TokenAuth']], function () {
+Route::group(['prefix' => '/index', 'namespace' => '', 'interceptor' => ['Auth']], function () {
 
     Route::post('/', 'IndexController::index');
+
+    Route::get('/', 'IndexController::index');
 });
 
 Route::get('/', 'IndexController::index', null, 'Auth');
+
+Route::post('/', 'IndexController::index', null, 'Auth');

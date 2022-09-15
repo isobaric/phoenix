e49fdb614a25901b3e72a40d72f6ecb47f8ec484
YHC <yanhaochuang@qq.com>
2022-09-15 16:34:41 +0800

    新增命令行执行程序的配置文件

--- Config/command.php
+++ Config/command.php
@@ -4,16 +4,16 @@ return [
     /**
      * key=>value格式，value为callable类型，value不接受参数，参数需要在命令行中传递
      *
+     * value格式：
+     * key => [Application\Controllers\IndexController::class, 'index']
+     * key => ['Application\Controller\IndexController', 'index']
+     * key => 'Application\Controllers\IndexController::index'
+     *
      * 不带参数：
      * php command test
      *
      * 带参数：参数之间以空格分割
      * php command test name=tom,lily age=12 file=old=new
      */
-    'test' => 'Application\Controllers\IndexController::index',
-
-    /**
-     * value的另一种形式
-     */
-    'other' => [Application\Controllers\IndexController::class, 'index'],
+    'test' => [Application\Controllers\IndexController::class, 'index']
 ];
